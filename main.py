40f10e3d475e8835ed3757e9944e498a3c21cb23
ulakshminarayana <41337335+ulakshminarayana@users.noreply.github.com>
2020-08-28 17:14:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
+import urllib.request
 import cv2
 import numpy as np
-import socket
-import struct
-from io import BytesIO
-from kivy.app import App
-from kivy.uix.button import Button
-
-class MainApp(App):
-    def build(self):
-        # Capture frame
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(('192.168.1.160', 8080))
-
-        while cap.isOpened():
-            _, frame = cap.read()
-
-            memfile = BytesIO()
-            np.save(memfile, frame)
-            memfile.seek(0)
-            data = memfile.read()
-
-            # Send form byte array: frame size + frame content
-            client_socket.sendall(struct.pack("L", len(data)) + data)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cap.release()
-        return Button(text="Hello World")
-
-
-MainApp().run()
+import time
+URL = "http://192.168.43.1:8080"
+while True:
+    img_arr = np.array(bytearray(urllib.request.urlopen(URL).read()),dtype=np.uint8)
+    img = cv2.imdecode(img_arr,-1)
+    cv2.imshow('IPWebcam',img)
+    
+    if cv2.waitKey(1):
+        break
